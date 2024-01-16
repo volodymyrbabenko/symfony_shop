e6a22067f05a0cbe95579c9c460f8f8b2d50a25f
Volodymyr <vladimirubabenko@gmail.com>
2024-01-16 10:37:30 +0100

    add in project files composer.lock and             package-lock.json

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,9 @@ Encore
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
 
+    // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
+    .enableStimulusBridge('./assets/controllers.json')
+
     .enableVueLoader()
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
